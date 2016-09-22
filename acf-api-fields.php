1545f9f10a416010f5de1d49dc557e3b343e6eaa
Lucas Stark <lucas.stark@live.com>
2016-09-22 09:44:36 -0400

    0.0.2 - Sort order

--- acf-api-fields.php
+++ acf-api-fields.php
@@ -4,7 +4,7 @@
   Plugin Name: Advanced Custom Fields: API Fields
   Plugin URI: https://github.com/lucasstark/acf-api-fields
   Description: WP Rest API Object and Relationship Fields
-  Version: 0.0.1
+  Version: 0.0.2
   Author: Lucas Stark
   Author URI: http://www.github.com/lucasstark/
   License: GPLv2 or later
--- inc/class-acf-api-fields-api.php
+++ inc/class-acf-api-fields-api.php
@@ -3,19 +3,21 @@
 class ACF_API_Fields_API {
 
 	/**
-	 * 
+	 *
 	 * @param array $field The ACF Field
 	 * @param array $args Query args to pass to the API
+	 *
 	 * @return \ACF_API_Fields_Model_Post[]
 	 */
 	public function get_posts( $field, $args ) {
 
 		$api_endpoint_url = add_query_arg( $args, $field['api_endpoint_url'] );
-		$response = wp_remote_get( $api_endpoint_url, array('timeout' => 20) );
+		$response         = wp_remote_get( $api_endpoint_url, array( 'timeout' => 20 ) );
 
 		if ( is_wp_error( $response ) ) {
 			//$this->debug( sprintf( __( 'HTTP request returned an error: %s (%s).', 'json-shortcode' ), $response->get_error_message(), $response->get_error_code() ) );
 			echo $response->get_error_message();
+
 			return;
 		}
 
@@ -31,15 +33,19 @@ class ACF_API_Fields_API {
 
 
 		$results = array();
+		foreach ( $args['include'] as $id ) {
+			$results[ $id ] = null;
+		}
+
 		$data = json_decode( $json_response );
 
 		foreach ( $data as $post_data ) {
-			
+
 			$model = apply_filters( 'acf_api_fields_get_api_model', false, $post_data, $field, $args );
-			if ( !empty( $model ) ) {
-				$results[] = $model;
+			if ( ! empty( $model ) ) {
+				$results[$post_data->id] = $model;
 			} else {
-				$results[] = new ACF_API_Fields_Model_Post( $api_endpoint_url, $post_data );
+				$results[$post_data->id] = new ACF_API_Fields_Model_Post( $api_endpoint_url, $post_data );
 			}
 		}
 
